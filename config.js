926941cc5e1c8345460b58d6b84f5f8d4736a735
Houssem Eddine Bousseliou <HOUSSEMBOUSSELIOU181192@GMAIL.COM>
2026-07-26 13:03:35 +0200

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-window.NARIS_CONFIG = {
-  supabaseUrl: "https://cyjprxuxletbfeqjmutj.supabase.co",
-  supabaseAnonKey: "sb_publishable_GaVqtCLxgfmiKPFuKT4NmQ_Vc-NOm9F",
-  whatsapp: "213552080462"
-};
